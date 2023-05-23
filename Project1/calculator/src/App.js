80a760b54cfa91af04048f3c0935dbf725e2570c
Sai Pranav <saipranav918@gmail.com>
2023-05-23 22:24:31 +0530

    Here is a commit

---
+++ Project1/calculator/src/App.js
@@ -0,0 +1,162 @@
+import Wrapper from"./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+import React,{ useState } from "react";
+
+
+const btnValue = [
+  ["C", "+/-", "%", "/"],
+  [7,8,9,"*"],
+  [4,5,6,"-"],
+  [1,2,3,"+"],
+  [0,".","="],
+];
+
+
+const toLocalString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+
+const removeSpaces = (num) => 
+  num.toString().replace(/\s/g, "");
+
+
+const App = () => {
+
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if(removeSpaces(calc.num).length < 16){
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0" ? "0"
+          : removeSpaces(calc.num) % 1 === 0 ? toLocalString(Number(removeSpaces(calc.num + value)))
+          : toLocalString(calc.num + value),
+        res: !calc.sign ? 0  : calc.res,
+      });
+    }
+  };
+
+
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num +value : calc.num,
+    });
+  };
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0, 
+    });
+  };
+
+  const equalsClickHandler = () => {
+    if(calc.sign && calc.num){
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+    
+
+    setCalc({
+      ...calc,
+      res:
+        calc.num === "0" && calc.num ==="/" ? "Cant divide with 0"
+        : toLocalString(
+            math(
+              Number(removeSpaces(calc.res)),
+              Number(removeSpaces(calc.num)),
+              calc.sign
+              )
+            ),
+      sign: "",
+      num: 0,
+    });
+    }
+  };
+
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? toLocalString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.num ? toLocalString(removeSpaces(calc.res) * -1) : 0,
+      sign: "",
+    });
+  };
+
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100,1)),
+      res: (res /= Math.pow(100,1)),
+      sign: "", 
+    });
+  };
+
+
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign : "",
+      num : 0,
+      res : 0,
+    });
+  };
+
+
+
+  return(
+    <Wrapper>
+      <Screen value={calc.num ? calc.num : calc.res} />
+      <ButtonBox>{
+        btnValue.flat().map((btn,i) =>{
+          return (
+            <Button
+              key = {i}
+              className={btn === "=" ? "equals" : ""}
+              value = {btn}
+              onClick={
+                btn === "C" ? resetClickHandler
+                : btn === "+/-" ? invertClickHandler
+                : btn === "%" ? percentClickHandler
+                : btn === "=" ? equalsClickHandler
+                : btn === "/" || btn === "X" || btn === "-" || btn === "+" ? signClickHandler
+                : btn === "." ? commaClickHandler
+                : numClickHandler
+              }
+              />
+          );
+        })
+        }
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
+export default App;
